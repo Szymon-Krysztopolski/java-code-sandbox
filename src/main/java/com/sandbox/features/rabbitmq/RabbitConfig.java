4bc74c1915211eafe4bae75f2286a07ee26af452
Technik <szymon.krysztopolski@gmail.com>
2026-03-06 12:23:30 +0100

    Add RabbitConfig

---
+++ src/main/java/com/sandbox/features/rabbitmq/RabbitConfig.java
@@ -0,0 +1,30 @@
+package com.sandbox.features.rabbitmq;
+
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.amqp.support.converter.MessageConverter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RabbitConfig {
+
+    @Bean
+    public Queue notificationQueue() {
+        return new Queue("myQueue", false);
+    }
+
+    @Bean
+    public MessageConverter jackson2MessageConverter() {
+        return new Jackson2JsonMessageConverter();
+    }
+
+    @Bean
+    public RabbitTemplate rabbitTemplate(ConnectionFactory connectionFactory, MessageConverter jackson2MessageConverter) {
+        RabbitTemplate template = new RabbitTemplate(connectionFactory);
+        template.setMessageConverter(jackson2MessageConverter);
+        return template;
+    }
+}
